a1019e968073015fcbc7639836fec72ec60b3bd1
Abdalr7man <bdwmhmd416@gmail.com>
2025-03-13 02:45:23 +0200

    Commit on 2025-03-13 02:45:23

--- Cli-Task-Tracker/main.py
+++ Cli-Task-Tracker/main.py
@@ -1,25 +1,66 @@
+#!python
+import json
 import cmd
-import pyfiglet 
 from model import User , Task
 
 
 class ToDoList(cmd.Cmd):
-    intro = "Welcom" # TODO : Use pyfiglet to add logo of (to do list app)
-    prompt = ">>"
+    intro = "Welcom To The To-Do_List" # TODO : Use pyfiglet to add logo of (to do list app)
+    prompt = ">> "
     
     def __init__(self):
-        ...
-        
+        super().__init__()
+        self.user = None 
+        self.do_login()
 
+    def do_login(self):
+        """Login to the To Do List Cli App"""
+        login = input("Do you have an account? (y/n): ").lower().strip()
+        if login == "n" :
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
+            user = User(username, password)
+            if not user :
+                print("valid name or password")
+                exit()
+            self.user = user
 
+        elif login == "y" : # TODO : Fix it 
+            if user.name == username and user.password == password :
+                self.user = user 
+            else :
+                print("valid input")
+                exit()
 
+    def do_add(self, arg):
+        """ Add a task to the task manager """
+        self.user.add_task(arg)
+    
+    def do_list(self, arg):
+        """ List all tasks in the task manager """
+        self.user.list_tasks()
 
+    def do_done(self, arg) :
+        """ List done tasks in the task manager """
+        self.user.done_tasks()
 
+    def do_inprogress(self, arg):
+        """ List inprogress tasks in the task manager """
+        self.user.inprogress()
 
+    def do_mark(self, arg):
+        """ Mark a task as completed or Inprogress"""
+        for task in self.user.tasks :
+            if task.name == arg :
+                task.edit_status()
 
+    def do_delete(self, arg):
+        """ Delete a task from the task manager """
+        self.user.delete_task(arg)
 
-
+    def do_exit(self, arg): # TODO : save user and Tasks before Close
+        return True
 
 
 if __name__ == "__main__" :
-    print("Hello")
+    ToDoList().cmdloop()
--- Cli-Task-Tracker/model.py
+++ Cli-Task-Tracker/model.py
@@ -1,38 +1,91 @@
+import json
 import uuid
 import time
-import json
-
 class User :
+    _FILEPATH = r"/home/abdalrhman/All/Programing & CS/My_Projects/Todo_list/To-Do-List/Cli-Task-Tracker/Data/users.json"
     def __init__(self, name, password):
         self.name = name 
         self.password = password
         self.id = str(uuid.uuid4()) # random 32 char according to RFC 4122
         self.tasks = []
+        self.is_there()
 
-    def add_task(self, task:str):
+    def is_there(self):
+        with open(self._FILEPATH, "r") as f :
+            file = f.read()
+            data = json.loads(file)
+            for user in data :
+                if self.name in user and self.password in user :
+                    return True
+                else :
+                    return False
+
+    def add_task(self, task_name:str, discreption_task:str = "No Discreption"):
+        task = Task(task_name, discreption_task)
         self.tasks.append(task)
 
+    def list_tasks(self):
+        for task in self.tasks :
+            print(task.name)
 
+    def done_tasks(self):
+        for task in self.tasks :
+            if task.status == "Done" :
+                print(task.name)
 
+    def inprogress(self):
+        for task in self.tasks :
+            if task.status == "Inprogress" :
+                print(task.name)
 
+    def not_done(self):
+        for task in self.tasks :
+            if task.status == "Not Done" :
+                print(task.name)
 
+    def delete_task(self, name_task) :
+        for i in self.tasks :
+            if i.name.lower() == name_task.lower() :
+                self.tasks.remove(i)
+                i = None
 class Task:
-    def __init__(self, name, discreption):
+    _FILEPATH = r"/home/abdalrhman/All/Programing & CS/My_Projects/Todo_list/To-Do-List/Cli-Task-Tracker/Data/tasks.json"
+    def __init__(self, name, discreption = "No Discreption"):
         self.name = name 
-        self.discreption = discreption
-        self.status = "to-do"
-        self.id = str(uuid.uuid4())
         self.created_at = time.ctime()
+        self.discreption = discreption
+        self.status = "Not Done"
 
-    def delet(self):
-        ...
+    def edit_discreption(self):
+        new_discreption = input(f"Write your new discreption for your task {self.name}:\n")
+        self.discreption = new_discreption
 
-    def edit(self):
-        ...
+    def edit_status(self):
+        new_status = input("write your new status --> Done , Inprogress, Not Done:\n")
+        if new_status.lower() == "done" :
+            self.status = "Done"
+        elif new_status.lower() == "inprogress" :
+            self.status = "Inprogress"
+        elif new_status.lower() == "not done" :
+            self.status = "Not Done"
 
-
+    def task_info(self):
+        return f"The task {self.name}\nIts created at {self.created_at}\nand The status is {self.status}"
 
 
 if __name__ == "__main__" :
     print(uuid.uuid4())
     print(time.ctime())
+    abdo = User("Abdalrhman", "2004/6/1//")
+    abdo.add_task("play game", "i will play pes")
+    abdo.add_task("study", "i will study math")
+    abdo.add_task("visit", "i will visit the Family")
+    print(abdo.tasks[0].task_info())
+    print(abdo.tasks[0].status)
+    # abdo.list_tasks()
+    abdo.delete_task("visit")
+    # abdo.list_tasks()
+    # abdo.tasks[0].edit_status()
+    abdo.done_tasks()
+    abdo.inprogress()
+    print(abdo.__dict__)
