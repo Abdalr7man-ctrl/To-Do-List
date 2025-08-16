754da0cbc49e158428e787daaad6f88d81c9110f
Abdalr7man <Eng.Abdalrhman.Abdalmonem@gmail.com>
2025-08-17 00:13:03 +0300

    API Init

--- API-Task-Tracker/server.py
+++ API-Task-Tracker/server.py
@@ -6,3 +6,22 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 PORT = 8080
 HOST = "localhost"
+
+class Server(BaseHTTPRequestHandler):
+
+
+    def do_GET(self):
+        ...
+
+    def do_POST(self):
+        ...
+
+    def do_PUT(self):
+        ...
+
+    def do_DELET(self):
+        ...
+
+with HTTPServer(HOST, PORT, Server) as myServer:
+    print(f"Start the server at http://{HOST}:{PORT}/")
+    myServer.serve_forever()
