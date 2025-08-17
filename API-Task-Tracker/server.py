9720db3caf0ad8f96f68a11f13ecb7c186efa076
Abdalr7man <Eng.Abdalrhman.Abdalmonem@gmail.com>
2025-08-18 02:11:44 +0300

    :)

--- API-Task-Tracker/server.py
+++ API-Task-Tracker/server.py
@@ -1,17 +1,55 @@
 """
 HTTP server and request handler
 """
+import json
 from urllib.parse import parse_qs, urlparse
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 PORT = 8080
 HOST = "localhost"
 
-class Server(BaseHTTPRequestHandler):
-
+class ToDoListServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        ...
+
+        myURL = urlparse(self.path)
+        myQu = parse_qs(myURL.query)
+
+        with open("./static/index.html", "r") as f:
+            htmlData = f.read()
+
+        with open("./static/style.css", "r") as f:
+            cssData = f.read()
+
+        if self.path == "/":
+            self.send_response(200, "Thats Very Good. :)")
+            self.send_header("content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(htmlData.encode())
+
+        elif self.path == "/tasks":
+            self.send_response(200, "Thats Very Good. :)")
+            self.send_header("content-type", "text/html")
+            self.end_headers()
+            # self.wfile.write(jsonData.endcode())
+
+        elif self.path == "/tasks?":
+            ...
+
+        elif self.path == "/style.css":
+            self.send_response(200, "Thats Very Good. :)")
+            self.send_header("content-type", "text/css")
+            self.end_headers()
+            self.wfile.write(cssData.encode())
+
+        elif self.path == "/favicon.ico":
+            ...
+
+        else:
+            self.send_response(404, "Fuck HTTP")
+            self.send_header("content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Error The page Not Found.".encode())
 
     def do_POST(self):
         ...
@@ -19,9 +57,13 @@ class Server(BaseHTTPRequestHandler):
     def do_PUT(self):
         ...
 
-    def do_DELET(self):
+    def do_DELETE(self):
         ...
 
-with HTTPServer(HOST, PORT, Server) as myServer:
-    print(f"Start the server at http://{HOST}:{PORT}/")
-    myServer.serve_forever()
+
+try:
+    with HTTPServer((HOST, PORT), ToDoListServer) as myServer:
+        print(f"Start the server at http://{HOST}:{PORT}/")
+        myServer.serve_forever()
+except KeyboardInterrupt:
+    print("\n\nServer is closed.")
